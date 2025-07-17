cc07bdc91f0030c2f9ae592b8492dcb3f1c444fc
samyakhirap18 <samyakvhirap@gmail.com>
2025-07-17 23:19:29 +0530

    Add files via upload

---
+++ lib/utils.ts
@@ -0,0 +1,112 @@
+export const categoryMenuList = [
+  {
+    id: 1,
+    title: "Casual Outfit",
+    src: "/casual.png",
+    href: "/shop/Casual-outfit"
+  },
+  {
+    id: 2,
+    title: "Night Outfit",
+    src: "/night.png",
+    href: "/shop/Night-Outfit"
+  },
+  {
+    id: 3,
+    title: "T Shirt Outfit",
+    src: "/night.png",
+    href: "/shop/T-Shirt-Outfit"
+  },
+  {
+    id: 4,
+    title: "Winter Outfits",
+    src: "/winter.png",
+    href: "/shop/Winter-Outfits"
+  },
+
+ 
+];
+
+export const incentives = [
+  {
+    name: "Free Shipping",
+    description:
+      "Our shipping is completely free and that is completely good for our customers.",
+    imageSrc: "/shipping icon.png",
+  },
+  {
+    name: "24/7 Customer Support",
+    description:
+      "Our support is working all day and night to answer any question you have.",
+    imageSrc: "/support icon.png",
+  },
+  {
+    name: "Fast Shopping Cart",
+    description:
+      "We have super fast shopping experience and you will enjoy it.",
+    imageSrc: "/fast shopping icon.png",
+  },
+];
+
+export const navigation = {
+  sale: [
+    { name: "Discounts", href: "#" },
+    { name: "News", href: "#" },
+    { name: "Register Discounts", href: "#" },
+  ],
+  about: [
+    { name: "About mensFyt", href: "#" },
+    { name: "Work With Us", href: "#" },
+    { name: "Company Profile", href: "#" },
+  ],
+  buy: [
+    { name: "MensFyt Card", href: "#" },
+    { name: "Terms Of Use", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "Complaints", href: "#" },
+    { name: "Partners", href: "#" },
+  ],
+  help: [
+    { name: "Contact", href: "#" },
+    { name: "How to Buy at mensfyt", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+};
+
+export const isValidNameOrLastname = (input: string) => {
+  // Simple name or lastname regex format check
+  const regex = /^[a-zA-Z\s]+$/;
+  return regex.test(input);
+};
+
+export const isValidEmailAddressFormat = (input: string) => {
+  // simple email address format check
+  const regex = /^\S+@\S+\.\S+$/;
+  return regex.test(input);
+};
+
+export const isValidCardNumber = (input: string) => {
+  // Remove all non-digit characters
+  const cleanedInput = input.replace(/[^0-9]/g, "");
+  // test for credit card number between 13 and 19 characters
+  const regex = /^\d{13,19}$/;
+  return regex.test(cleanedInput);
+}
+
+export const isValidCreditCardExpirationDate = (input: string) => {
+  // simple expiration date format check
+  const regex = /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/;
+  return regex.test(input);
+};
+
+export const isValidCreditCardCVVOrCVC = (input: string) => {
+  // simple CVV or CVC format check
+  const regex = /^[0-9]{3,4}$/;
+  return regex.test(input);
+};
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
